5094bf83732bfe75e955a2173de3c4f8f6bc5977
rave-roses <odekunlewaris01@gmail.com>
2024-10-07 00:22:06 +0100

    Navs with hover border added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+
 function LogoName() {
   return <div className="logoName">Titilope Sneakers</div>;
 }
 
 function Navs() {
+  const mouse0ver = (e) => {
+    e.target.style.paddingBottom = "40px";
+    e.target.style.borderBottom = "2px solid hsl(26, 100%, 55%)";
+  };
+  const mouse0ut = (e) => {
+    e.target.style.paddingBottom = "";
+    e.target.style.borderBottom = "";
+  };
+
   return (
     <div className="navs">
       <LogoName />
       <ul>
-        <li>Collections</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <a href="/" onMouseOver={mouse0ver} onMouseOut={mouse0ut}>
+            Collections
+          </a>
+        </li>
+        <li>
+          <a href="/" onMouseOver={mouse0ver} onMouseOut={mouse0ut}>
+            Men
+          </a>
+        </li>
+        <li>
+          <a href="/" onMouseOver={mouse0ver} onMouseOut={mouse0ut}>
+            Women
+          </a>
+        </li>
+        <li>
+          <a href="/" onMouseOver={mouse0ver} onMouseOut={mouse0ut}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/" onMouseOver={mouse0ver} onMouseOut={mouse0ut}>
+            Contact
+          </a>
+        </li>
       </ul>
     </div>
   );
